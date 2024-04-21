632619e61c177cc369cbb1f713db2f65cbdfd0e2
Baullin1767 <86959616+Baullin1767@users.noreply.github.com>
2024-04-21 23:15:41 +0300

    Добавил локализаию, сохранение в YG, поправил рекламу

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using DG.Tweening;
+using DG.Tweening.Plugins.Core.PathCore;
+using System;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
+using YG;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,12 +21,44 @@ public class GameManager : MonoBehaviour
     int phraseInterval;
 
     AudioSource audioSource;
+
+    string path_to_phrases_file;
+    [Serializable]
+    private class PhrasesContainer
+    {
+        public string[] phrases;
+        public string[] startPhrases;
+    }
+
+    private void OnEnable() => YandexGame.GetDataEvent += GetLoad;
+    private void OnDisable() => YandexGame.GetDataEvent -= GetLoad;
+
     private void Awake()
     {
-        score = PlayerPrefs.GetInt("Score");
+        path_to_phrases_file = Application.dataPath;
+        if (Application.systemLanguage == SystemLanguage.English)
+        {
+            path_to_phrases_file += "/phrases_eng.json";
+        }
+        else if (Application.systemLanguage == SystemLanguage.Russian)
+        {
+            path_to_phrases_file += "/phrases_ru.json";
+        }
+
+        if (File.Exists(path_to_phrases_file))
+        {
+            string json = File.ReadAllText(path_to_phrases_file);
+            PhrasesContainer container = JsonUtility.FromJson<PhrasesContainer>(json);
+            phrases = container.phrases;
+            startPhrases = container.startPhrases;
+        }
+
+        if (YandexGame.SDKEnabled == true)
+        {
+            GetLoad();
+        }
         scoreText.text = score.ToString();
-        phraseInterval = score + Random.Range(30, 40);
-        index = PlayerPrefs.GetInt("Index");
+        phraseInterval = score + UnityEngine.Random.Range(30, 40);
         audioSource = GetComponent<AudioSource>();
     }
 
@@ -35,8 +69,14 @@ public class GameManager : MonoBehaviour
             .Subscribe(_ => { RandomText(); });
         if(score > 0)
         {
-            phrasesText.text = startPhrases[Random.Range(0, startPhrases.Length)];
+            phrasesText.text = startPhrases[UnityEngine.Random.Range(0, startPhrases.Length)];
         }
+    }
+
+    public void GetLoad()
+    {
+        score = YandexGame.savesData.score;
+        index = YandexGame.savesData.index;
     }
 
     public void OnClick()
@@ -44,20 +84,23 @@ public class GameManager : MonoBehaviour
         score++;
         phraseEvent.OnNext(score);
         scoreText.text = score.ToString();
-        audioSource.PlayOneShot(soundsClick[Random.Range(0, soundsClick.Length)]);
+        audioSource.PlayOneShot(soundsClick[UnityEngine.Random.Range(0, soundsClick.Length)]);
     }
     public void RandomText() 
     {
         phrasesText.text = phrases[index];
         index++;
-        PlayerPrefs.SetInt("Index", index);
-        PlayerPrefs.SetInt("Score", score);
-        if(index == phrases.Length)
+
+        YandexGame.savesData.index = index;
+        YandexGame.savesData.score = score;
+        YandexGame.SaveProgress();
+
+        if (index == phrases.Length)
         {
             index = 0;
         }
         scoreText.transform.DOShakeScale(0.15f, 1f, 10, 90f, true, ShakeRandomnessMode.Harmonic);
         phrasesText.transform.DOShakeScale(0.15f, 1f, 10, 90f, true, ShakeRandomnessMode.Harmonic);
-        phraseInterval += Random.Range(30, 50);
+        phraseInterval += UnityEngine.Random.Range(30, 50);
     }
 }
