c75285b2a1c64cc970f70b7fdbb07606b831545e
Baullin1767 <86959616+Baullin1767@users.noreply.github.com>
2024-04-21 14:46:27 +0300

    Add YG plugin, Add Ad

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,42 @@
 using DG.Tweening;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Text scoreText;
-    public Text phrasesText;
+    [SerializeField] Text scoreText;
+    [SerializeField] Text phrasesText;
     int score;
     Subject<int> phraseEvent = new Subject<int>();
     int index;
-    public string[] phrases;
+    [SerializeField] string[] phrases;
+    [SerializeField] string[] startPhrases;
+    [SerializeField] AudioClip[] soundsClick;
 
     int phraseInterval;
 
-    IShowAd showAd;
+    AudioSource audioSource;
+    private void Awake()
+    {
+        score = PlayerPrefs.GetInt("Score");
+        scoreText.text = score.ToString();
+        phraseInterval = score + Random.Range(30, 40);
+        index = PlayerPrefs.GetInt("Index");
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void Start()
     {
-        phraseInterval = Random.Range(40, 60);
         phraseEvent
             .Where(_ => score >= phraseInterval)
             .Subscribe(_ => { RandomText(); });
+        if(score > 0)
+        {
+            phrasesText.text = startPhrases[Random.Range(0, startPhrases.Length)];
+        }
     }
 
     public void OnClick()
@@ -29,18 +44,20 @@ public class GameManager : MonoBehaviour
         score++;
         phraseEvent.OnNext(score);
         scoreText.text = score.ToString();
+        audioSource.PlayOneShot(soundsClick[Random.Range(0, soundsClick.Length)]);
     }
     public void RandomText() 
     {
         phrasesText.text = phrases[index];
         index++;
+        PlayerPrefs.SetInt("Index", index);
+        PlayerPrefs.SetInt("Score", score);
         if(index == phrases.Length)
         {
             index = 0;
         }
         scoreText.transform.DOShakeScale(0.15f, 1f, 10, 90f, true, ShakeRandomnessMode.Harmonic);
         phrasesText.transform.DOShakeScale(0.15f, 1f, 10, 90f, true, ShakeRandomnessMode.Harmonic);
-        phraseInterval = Random.Range(40, 60);
-        showAd.ShowAdd();
+        phraseInterval += Random.Range(30, 50);
     }
 }
---
+++ Assets/Scripts/YGADManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class YGADManager : MonoBehaviour, IShowAd
+{
+    public void ShowAdd()
+    {
+        
+    }
+}
